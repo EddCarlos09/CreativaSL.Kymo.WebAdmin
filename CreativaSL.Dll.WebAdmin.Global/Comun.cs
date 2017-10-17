bf86ef25e4ba1ab9f8ed2eb2022c873b561fd935
EnriqueMB <kikeballina1@gmail.com>
2017-10-17 18:18:09 -0500

    Comun

--- CreativaSL.Dll.WebAdmin.Global/Comun.cs
+++ CreativaSL.Dll.WebAdmin.Global/Comun.cs
@@ -9,15 +9,19 @@ namespace CreativaSL.Dll.WebAdmin.Global
     public static class Comun
     {
         private static string _Conexion;
-
+        /// <summary>
+        /// Conexion la el acceso a la base de datos
+        /// </summary>
         public static string Conexion
         {
             get { return _Conexion; }
             set { _Conexion = value; }
         }
-
+        
         private static string _IDUsuario;
-
+        /// <summary>
+        /// IDUsuario es el usuario que se encuentra logeado en nuestro sistema
+        /// </summary>
         public static string IDUsuario
         {
             get { return _IDUsuario; }
