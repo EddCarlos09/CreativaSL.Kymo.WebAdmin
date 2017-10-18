b252107c4c3b7b1d636f11008b5fd9b97fbb455c
EnriqueMB <kikeballina1@gmail.com>
2017-10-18 08:35:19 -0500

    ContactoGeneral

--- CreativaSL.Dll.WebAdmin.Global/EM_ContactoGeneral.cs
+++ CreativaSL.Dll.WebAdmin.Global/EM_ContactoGeneral.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -121,5 +122,66 @@ namespace CreativaSL.Dll.WebAdmin.Global
             set { _HostText = value; }
         }
 
+        #region DatosDeControl
+        private string _Conexion;
+        /// <summary>
+        /// Es la para asignar la cadena de conexion
+        /// </summary>
+        public string Conexion
+        {
+            get { return _Conexion; }
+            set { _Conexion = value; }
+        }
+
+        private int _Resultado;
+        /// <summary>
+        /// Es si la operacion que se realiza de vuelve un valor
+        /// </summary>
+        public int Resultado
+        {
+            get { return _Resultado; }
+            set { _Resultado = value; }
+        }
+
+        private bool _Completado;
+        /// <summary>
+        /// Es para ver si la operacion que se realizo es correcta
+        /// </summary>
+        public bool Completado
+        {
+            get { return _Completado; }
+            set { _Completado = value; }
+        }
+
+        private string _IDUsuario;
+        /// <summary>
+        /// Es para signar el usuario que se encuentra logeado en nuestro sistema
+        /// </summary>
+        public string IDUsuario
+        {
+            get { return _IDUsuario; }
+            set { _IDUsuario = value; }
+        }
+
+        private DataTable _TablaDatos;
+        /// <summary>
+        /// ???
+        /// </summary>
+        public DataTable TablaDatos
+        {
+            get { return _TablaDatos; }
+            set { _TablaDatos = value; }
+        }
+
+        private bool _NuevoRegistro;
+        /// <summary>
+        /// Es para verificacion de un nuevo registro
+        /// </summary>
+        public bool NuevoRegistro
+        {
+            get { return _NuevoRegistro; }
+            set { _NuevoRegistro = value; }
+        }
+        #endregion
     }
 }
