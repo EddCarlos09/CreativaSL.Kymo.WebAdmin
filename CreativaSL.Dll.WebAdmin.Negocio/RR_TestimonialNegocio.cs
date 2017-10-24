9a69cf71a39fa6b96009d9aa5fe010279718275c
Rafael Rosales <rafaeltux@hotmail.com>
2017-10-24 12:02:33 -0500

    testimoniales

---
+++ CreativaSL.Dll.WebAdmin.Negocio/RR_TestimonialNegocio.cs
@@ -0,0 +1,32 @@
+﻿using CreativaSL.Dll.WebAdmin.Datos;
+using CreativaSL.Dll.WebAdmin.Global;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreativaSL.Dll.WebAdmin.Negocio
+{
+    public class RR_TestimonialNegocio
+    {
+        public List<RR_Testimoniales> ObtenerTestimonial(RR_Testimoniales Datos)
+        {
+            RR_TestimonialesDatos TD = new RR_TestimonialesDatos();
+            return TD.ObtenerTestimoniales(Datos);
+        }
+
+        public void ObtenerTestimonialXId(RR_Testimoniales Datos)
+        {
+            RR_TestimonialesDatos TD = new RR_TestimonialesDatos();
+            TD.ObtenerTestimonialesXID(Datos);
+        }
+
+        public void ActivarTestimonial(RR_Testimoniales Datos)
+        {
+            RR_TestimonialesDatos TD = new RR_TestimonialesDatos();
+            TD.ActivarTestimonial(Datos);
+        }
+
+    }
+}
